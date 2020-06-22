36de709e0a4c96087aece570d90657cf04965264
ertanyildiz <ertanyildizz@gmail.com>
2020-06-22 16:38:33 +0300

    tr > createUserWithEmailAndPassword.js added

--- tr/auth/createUserWithEmailAndPassword.js
+++ tr/auth/createUserWithEmailAndPassword.js
@@ -1,10 +1,3 @@
-# Firebase custom errors
-
-This repo aims to show local error messages to user when an exception occurs. Feel 
-free to add  translation of your language.
-
-## sample usage
-```js
 const firebaseErrors = {
     'auth/user-not-found': 'Bu emaile ait herhangi bir kullanıcı bulunamadı!',
     'auth/email-already-in-use': 'Bu email adresi daha sistemde mevcut',
@@ -12,9 +5,3 @@ const firebaseErrors = {
     'auth/operation-not-allowed': 'Email-Şifre kayıt işlemi için izin verilmedi.',
     'auth/weak-password': 'Girilen şifre yeterince güçlü değil'
   };
-  auth.createUserWithEmailAndPassword("mailaddress@example.com", "123456").then(cred => {
-      console.log(cred.user);
-  }).catch(error => {
-     alert(firebaseErrors[error.code] || error.message);
-  })
-```
